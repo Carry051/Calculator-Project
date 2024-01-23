f77b2fef8997382f9a911f1ee20613ab7e98148c
Maksym <mvoron@gmail.com>
2024-01-23 09:38:58 +0100

    change JS

--- script.js
+++ script.js
@@ -13,15 +13,6 @@ buttonsPanel.addEventListener('click', (e) => {
 
     case '=':
       const result = new Function('return ' + out.innerText)();
-      const expression = out.innerText;
-
-      // Перевірка наявності конкретного рядка
-      if (expression === '10.10+21+18') {
-        out.innerText = ''
-        displayLoveMessage()
-        break;
-      }
-
       out.innerText = result.toFixed(0)
       break;
 
@@ -31,18 +22,3 @@ buttonsPanel.addEventListener('click', (e) => {
       }
   }
 });
-
-
-function displayLoveMessage() {
-  const message = 'Я тебе кохаю, Киця';
-
-  // Розділити рядок на масив букв
-  const letters = message.split('');
-
-  // Вивести кожну букву на новому рядку
-  letters.forEach((letter, index) => {
-    setTimeout(() => {
-      out.innerHTML += letter + ''
-    }, index * 500); // Кожну букву виводити з проміжком у 500 мілісекунд
-  });
-}
